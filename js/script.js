e67a530a184bd063dad00b00d47b5355a7a06529
Anandpskerala <keralapyapi@gmail.com>
2024-09-13 11:28:02 +0530

    Added from validation

--- js/script.js
+++ js/script.js
@@ -13,4 +13,35 @@ const loadSideBar = (isMenu) => {
         sideBar.style.display = "none";
         sideMenu.innerHTML = '<span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg></span>';
     }
-}
+};
+
+$(document).ready(function() {
+    $("#contact-form").submit((e) => {
+        e.preventDefault();
+        var name = document.getElementById("name").value;
+        var email = document.getElementById("email").value;
+        var message = document.getElementById("message").value;
+        const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-z]{2,6}$/;
+
+        if (name == "" || name == null || name.length < 4) {
+            alert("Please Provide a valid name with atleast 4 characters");
+        } else if (email == "" || email == null || !re.test(String(email).toLowerCase())) {
+            alert("Please provide a valid email");
+        } else if (message == "" || message == null || message.length <10) {
+            alert("Please Provide a valid message. Include atleat 10 characters");
+        } else {
+            $.ajax({
+                url: "https://script.google.com/macros/s/AKfycbwHopK8dXMuZ2rMjLO-qBwCq-FZCFc6ov_FLKNXnRPZ2rFSJ-rQ1loCbHAVBLxjhmqi/exec",
+                method: "post",
+                data: $("#contact-form").serialize(),
+                success: function(response) {
+                    alert("Sucessfully submitted form.");
+                    window.location.reload();
+                },
+                error: function(err) {
+                    alert("Something wen wrong");
+                }
+            });
+        }
+    });
+});
